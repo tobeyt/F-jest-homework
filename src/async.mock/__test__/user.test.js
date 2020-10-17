6fff67073d4cfca221dbe4d8f3f64e05fe35a644
di.qin <di.qin@thoughtworks.com>
2020-10-17 17:17:04 +0800

    [QD]: finish all.

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,14 +1,20 @@
 import { register } from "../user";
+import { verifyPassword, verifyUsername } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
-    // TODO 19: add test here
+  test("should post user when validated", async () => {
+    const result = register("username", "password");
+    await expect(result).resolves.toEqual({});
+    expect(verifyUsername).toHaveBeenCalledTimes(1);
+    expect(verifyPassword).toHaveBeenCalledTimes(1);
   });
 
-  test("should reject with Error when username is invalid", () => {
-    // TODO 20: add test here
+  test("should reject with Error when username is invalid", async () => {
+    verifyUsername.mockReturnValue(false);
+    const result = register("username", "password");
+    await expect(result).rejects.toThrow("wrong username or password");
   });
 });
--- src/mock.class/__test__/vaccineTest.test.js
+++ src/mock.class/__test__/vaccineTest.test.js
@@ -2,26 +2,47 @@ import VaccineTest from "../vaccineTest";
 import Recipient from "../recipient";
 import Covid19Vaccine from "../covid19Vaccine";
 
+const mockAcceptInjection = jest.fn();
+const mockGetHasAntibodies = jest.fn();
+
 jest.mock("../recipient", () => {
   // mock class实现
+  return jest.fn().mockImplementation(() => ({
+    acceptInjection: mockAcceptInjection,
+    getHasAntibodies: mockGetHasAntibodies,
+  }));
 });
 
 beforeEach(() => {
   // clear mock here
+  Recipient.mockClear();
+  mockAcceptInjection.mockClear();
+  mockGetHasAntibodies.mockClear();
 });
 
 describe("inject", () => {
   test("should recipient accept injection with vaccine", () => {
-    // TODO 14: add test here
+    const vaccine = new VaccineTest();
+    vaccine.inject();
+    expect(mockAcceptInjection).toHaveBeenCalledWith(new Covid19Vaccine());
+    expect(mockAcceptInjection).toHaveBeenCalledTimes(1);
   });
 });
 
 describe("test", () => {
   test("should get Success if recipient has antibodies", () => {
-    // TODO 15: add test here
+    const vaccine = new VaccineTest();
+    mockGetHasAntibodies.mockReturnValue(true);
+    const result = vaccine.test();
+    expect(result).toBe("Vaccine Test Success");
+    expect(mockGetHasAntibodies).toHaveBeenCalledTimes(1);
   });
 
   test("should get Failed if recipient has no antibodies", () => {
-    // TODO 16: add test here
+    const vaccine = new VaccineTest();
+    mockGetHasAntibodies.mockReturnValue(false);
+    const result = vaccine.test();
+    expect(result).toBe("Vaccine Test Failed");
+    expect(mockGetHasAntibodies).toHaveBeenCalledTimes(1);
   });
 });
